2ef38ae544a7ce7dbc10f1ffe999c1569ffc8b2b
ritik kumar <ritikkumarkashyap9@gmail.com>
2025-05-19 15:57:01 +0530

    handling action based updtes like change-password

--- src/main/java/com/codewithmosh/store/controller/UserController.java
+++ src/main/java/com/codewithmosh/store/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.controller;
 
+import com.codewithmosh.store.Dtos.ChangePasswordRequest;
 import com.codewithmosh.store.Dtos.UserDto;
 import com.codewithmosh.store.Dtos.UserRegisterRequest;
 import com.codewithmosh.store.Dtos.UserUpdateRequest;
@@ -8,6 +9,7 @@ import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -101,6 +103,26 @@ public List<UserDto> getAllUsers(
     userRepository.delete(user);
   return ResponseEntity.noContent().build();
 
+    }
+
+    @PostMapping("/{id}/change-password")
+    public ResponseEntity<Void> changePassword(
+            @PathVariable Long id,
+            @RequestBody ChangePasswordRequest request
+    ) {
+        var user = userRepository.findById(id).orElse(null);
+
+        if(user == null){
+            return  ResponseEntity.notFound().build();
+        }
+
+        if(!user.getPassword().equals(request.getOldPassword())){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
+        return ResponseEntity.noContent().build();
     }
 
 }
