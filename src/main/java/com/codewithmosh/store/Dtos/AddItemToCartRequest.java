96a9a045691fa68741e356609ba95f19a9d86f42
ritik kumar <ritikkumarkashyap9@gmail.com>
2025-05-20 15:31:19 +0530

    add a product to the cart

---
+++ src/main/java/com/codewithmosh/store/Dtos/AddItemToCartRequest.java
@@ -0,0 +1,9 @@
+package com.codewithmosh.store.Dtos;
+
+import lombok.Data;
+
+@Data
+public class AddItemToCartRequest {
+
+    private Long productId;
+}
--- src/main/java/com/codewithmosh/store/controller/CartController.java
+++ src/main/java/com/codewithmosh/store/controller/CartController.java
@@ -1,15 +1,19 @@
 package com.codewithmosh.store.controller;
 
+import com.codewithmosh.store.Dtos.AddItemToCartRequest;
 import com.codewithmosh.store.Dtos.CartDto;
+import com.codewithmosh.store.Dtos.CartItemDto;
 import com.codewithmosh.store.entities.Cart;
+import com.codewithmosh.store.entities.CartItem;
 import com.codewithmosh.store.mappers.CartMapper;
 import com.codewithmosh.store.repositories.CartRepository;
+import com.codewithmosh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 
 @AllArgsConstructor
@@ -19,7 +23,7 @@ public class CartController {
 
     private final CartRepository cartRepository;
     private final CartMapper cartMapper;
-
+    private final ProductRepository productRepository;
 
 
     @PostMapping
@@ -30,6 +34,45 @@ public class CartController {
 
     return new ResponseEntity<>(cartDto ,HttpStatus.CREATED);
 
+
+    }
+
+    @PostMapping("/{cartId}/items")
+    public ResponseEntity<CartItemDto> addToCart(
+            @PathVariable UUID cartId,
+            @RequestBody AddItemToCartRequest request
+
+    ) {
+        var cart = cartRepository.findById(cartId).orElse(null);
+        if (cart == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+     var product = productRepository.findById(request.getProductId()).orElse(null);
+        if (product == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+      var cartItem =   cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(product.getId()))
+                .findFirst()
+                .orElse(null);
+
+
+        if(cartItem != null) {
+
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        }
+        else {
+            cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(cart);
+            cart.getCartItems().add(cartItem);
+        }
+        cartRepository.save(cart);
+ var cartItemDto = cartMapper.toCartItemDto(cartItem);
+       return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
 
     }
 
