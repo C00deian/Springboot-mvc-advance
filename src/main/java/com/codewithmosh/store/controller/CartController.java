9cbf07f9dd4cb447bc9b8eabdaea08b3aa3580fd
ritik kumar <ritikkumarkashyap9@gmail.com>
2025-05-20 16:46:39 +0530

    getting a cart

--- src/main/java/com/codewithmosh/store/controller/CartController.java
+++ src/main/java/com/codewithmosh/store/controller/CartController.java
@@ -43,7 +43,7 @@ public class CartController {
             @RequestBody AddItemToCartRequest request
 
     ) {
-        var cart = cartRepository.findById(cartId).orElse(null);
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
         if (cart == null) {
             return ResponseEntity.notFound().build();
         }
@@ -53,7 +53,7 @@ public class CartController {
             return ResponseEntity.badRequest().build();
         }
 
-      var cartItem =   cart.getCartItems().stream()
+      var cartItem =   cart.getItems().stream()
                 .filter(item -> item.getProduct().getId().equals(product.getId()))
                 .findFirst()
                 .orElse(null);
@@ -68,7 +68,7 @@ public class CartController {
             cartItem.setProduct(product);
             cartItem.setQuantity(1);
             cartItem.setCart(cart);
-            cart.getCartItems().add(cartItem);
+            cart.getItems().add(cartItem);
         }
         cartRepository.save(cart);
  var cartItemDto = cartMapper.toCartItemDto(cartItem);
@@ -76,5 +76,17 @@ public class CartController {
 
     }
 
+
+    @GetMapping("/{cartId}")
+    public ResponseEntity<CartDto> getCart(@PathVariable UUID cartId) {
+
+       var cart =  cartRepository.getCartWithItems(cartId).orElse(null);
+       if (cart == null) {
+           return ResponseEntity.notFound().build();
+       }
+
+       return ResponseEntity.ok(cartMapper.toCartDto(cart));
+
+    }
 
 }
--- src/main/java/com/codewithmosh/store/mappers/CartMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/CartMapper.java
@@ -10,6 +10,8 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface CartMapper {
 
+//   @Mapping(target = "items"  ,source = "items")
+    @Mapping(target = "totalPrice" , expression = "java(cart.getTotalPrice())")
     CartDto toCartDto(Cart cart);
     @Mapping(target = "totalPrice" ,expression = "java(cartItem.getTotalPrice())")
     CartItemDto toCartItemDto(CartItem cartItem);
