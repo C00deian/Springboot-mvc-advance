ba0dd41232df81ab39115ed4b71e73ebf1fd77f5
ritik kumar <ritikkumarkashyap9@gmail.com>
2025-05-21 13:55:22 +0530

    added a swagger UI for Api

--- src/main/java/com/codewithmosh/store/controller/CartController.java
+++ src/main/java/com/codewithmosh/store/controller/CartController.java
@@ -7,6 +7,7 @@ import com.codewithmosh.store.Dtos.UpdateCartItemRequest;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.ProductNotFoundException;
 import com.codewithmosh.store.services.CartService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/carts")
+@Tag(name = "Carts")
 public class CartController {
 
     private final CartService cartService;
