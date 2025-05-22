ae139ed0eb12062266b7af24f9bbcc6fa722744c
ritik kumar <ritikkumarkashyap9@gmail.com>
2025-05-22 11:16:59 +0530

    authentication using AuthenticationManager

--- src/main/java/com/codewithmosh/store/controller/AuthController.java
+++ src/main/java/com/codewithmosh/store/controller/AuthController.java
@@ -7,6 +7,9 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -16,22 +19,38 @@ import java.util.Map;
 @RequestMapping("/auth")
 public class AuthController {
 
-  private final  AuthService authService;
+//    private final AuthService authService;
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(
             @Valid @RequestBody LoginRequest loginRequest
     ){
-        authService.login(loginRequest);
+//        authService.login(loginRequest);
+          authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+
+        );
         return ResponseEntity.ok().build();
     }
 
 
-    @ExceptionHandler(UnauthorizedUserException.class)
-    public ResponseEntity<Map<String, String>> handleUnauthorizedUserException(){
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                Map.of("message", "Unauthorized User")
-        );
+//    @ExceptionHandler(UnauthorizedUserException.class)
+//    public ResponseEntity<Map<String, String>> handleUnauthorizedUserException(){
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+//                Map.of("message", "Unauthorized User")
+//        );
+//
+//    }
 
+
+//   mosh
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
+
 }
