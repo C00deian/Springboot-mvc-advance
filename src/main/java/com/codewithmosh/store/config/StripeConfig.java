9fb4c379215133dc059c4927f36242f602719158
ritik kumar <ritikkumarkashyap9@gmail.com>
2025-05-25 18:25:04 +0530

    initialize Stripe

---
+++ src/main/java/com/codewithmosh/store/config/StripeConfig.java
@@ -0,0 +1,19 @@
+package com.codewithmosh.store.config;
+
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Configuration
+public class StripeConfig {
+
+    @Value("${stripe.secretKey}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init(){
+        Stripe.apiKey = secretKey;
+    }
+}
